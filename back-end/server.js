b0384f3c7efa23300a4419c584f6fa8e0959c45a
Blaine Johnson <lindonbej@gmail.com>
2020-04-15 17:24:24 -0600

    changed server port

--- back-end/server.js
+++ back-end/server.js
@@ -227,4 +227,4 @@ app.delete('/api/posts/:blogID/:postID', async (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
